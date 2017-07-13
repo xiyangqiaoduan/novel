637f12dc3290ae5226b60dc7a6e82c6324b22c96
yangcb <yangcb@hui10.com>
2017-07-13 18:20:37 +0800

    新增DAO

---
+++ src/main/java/com/taototao/novel/dao/ArticleDao.java
@@ -0,0 +1,10 @@
+package com.taototao.novel.dao;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author yangcb
+ * @create 2017-07-13 15:33
+ **/
+public interface ArticleDao {
+}
---
+++ src/main/java/com/taototao/novel/dao/ChapterDao.java
@@ -0,0 +1,10 @@
+package com.taototao.novel.dao;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author yangcb
+ * @create 2017-07-13 17:44
+ **/
+public interface ChapterDao {
+}
